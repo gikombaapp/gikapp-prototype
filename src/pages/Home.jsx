31d24a7ac447d7d568dabde6accf30a5e110421a
kibetnathan <kibetnat@gmail.com>
2026-01-17 13:49:54 +0300

    add buttons to the hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 function Home() {
   return (
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        // Hero section
+        <div className="flex flex-col justify-center w-full h-[45vh] bg-[#0098B3] pl-25">
+            <h1 className="text-5xl font-bold text-[#BEFF72]">The Gikomba app project</h1>
+            <h2 className="text-3xl font-light italic text-[#BEFF72]">[Prototype Ver]</h2>
+            <div>
+            <button className="bg-[#BEFF72] text-[#0098B3] p-4 m-3 rounded-2xl hover:cursor-pointer outline-2 hover:outline-[#BEFF72] hover:text-[#BEFF72] hover:bg-[#0098B3]">Shop Now</button>
+            <button className="bg-[#0098B3] text-[#BEFF72] p-4 m-3 rounded-2xl hover:cursor-pointer outline-2 outline-[#BEFF72] hover:text-[#0098B3] hover:bg-[#BEFF72]">Start Selling</button>
+            </div>
+            
+        </div>
   );
 }
 
