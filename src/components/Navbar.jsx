e6f8ca11174498f9d551db8b531df090eb6bdd08
kibetnathan <kibetnat@gmail.com>
2026-01-20 05:31:31 +0300

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="flex flex-col bg-[#F3F0F8] w-full">
       <div className="flex flex-row bg-[#F3F0F8]  px-3 py-5 w-full justify-around items-center">
-        <span className="text-2xl text-[#8B60C3] font-bold">GikApp</span>
+        <span className="text-2xl text-[#8B60C3] font-bold">Gikomba.App</span>
         <input type="text" className="w-[40vw] h-fit py-2 pl-2 rounded-2xl outline outline-[#b5b2ba] bg-[#eae8ee]" placeholder="Search Bar"/> 
         <ul className="flex relative right-4">
           <li className="inline pr-5">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import ProductCard from "../components/ProductCard";
 
 function Home() {
   return (
+    <>
         // Hero section
-        <div className="flex flex-col justify-center w-full h-[45vh] bg-[#0098B3] pl-25">
+        <header className="flex flex-col justify-center w-full h-[45vh] bg-[#0098B3] pl-25">
             <h1 className="text-5xl font-bold text-[#BEFF72]">The Gikomba app project</h1>
-            <h2 className="text-3xl font-light italic text-[#BEFF72]">[Prototype Ver]</h2>
+            <h2 className="text-3xl font-light italic text-[#BEFF72]">[Prototype Version]</h2>
             <div>
             <button className="bg-[#BEFF72] text-[#0098B3] p-4 m-3 rounded-2xl hover:cursor-pointer outline-2 hover:outline-[#BEFF72] hover:text-[#BEFF72] hover:bg-[#0098B3]">Shop Now</button>
             <button className="bg-[#0098B3] text-[#BEFF72] p-4 m-3 rounded-2xl hover:cursor-pointer outline-2 outline-[#BEFF72] hover:text-[#0098B3] hover:bg-[#BEFF72]">Start Selling</button>
             </div>
-            
+        </header>
+        <div>
+            <ProductCard/>
         </div>
+    </>
   );
 }
 
