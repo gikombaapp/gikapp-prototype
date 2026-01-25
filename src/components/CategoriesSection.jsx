9ebc7743a5042894a5fa72d0a24cb799a36488dc
kibetnathan <kibetnat@gmail.com>
2026-01-25 11:09:59 +0300

    add shadows to category cards

--- src/components/CategoriesSection.jsx
+++ src/components/CategoriesSection.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 
 function CategoriesSection() {
   return (
-    <div className='flex flex-row flex-wrap w-full h-[45vh] bg-[#F3F0F8] justify-around items'>
-        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer'>
+    <div className='flex flex-row flex-wrap w-full h-fit bg-[#F3F0F8] justify-around items'>
+        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer hover:shadow-[2px_2px_15px] hover:shadow-[#0098B3]'>
             {/* Section Card */}
             <h3 className='text-[#BEFF72] text-2xl font-bold tracking-wider'>Category Name</h3>
         </div>
-        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer'>
+        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer hover:shadow-[2px_2px_15px] hover:shadow-[#0098B3]'>
             {/* Section Card */}
             <h3 className='text-[#BEFF72] text-2xl font-bold tracking-wider'>Category Name</h3>
         </div>
-        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer'>
+        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer hover:shadow-[2px_2px_15px] hover:shadow-[#0098B3]'>
             {/* Section Card */}
             <h3 className='text-[#BEFF72] text-2xl font-bold tracking-wider'>Category Name</h3>
         </div>
-        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer'>
+        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer hover:shadow-[2px_2px_15px] hover:shadow-[#0098B3]'>
             {/* Section Card */}
             <h3 className='text-[#BEFF72] text-2xl font-bold tracking-wider'>Category Name</h3>
         </div>
-        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer'>
+        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer hover:shadow-[2px_2px_15px] hover:shadow-[#0098B3]'>
             {/* Section Card */}
             <h3 className='text-[#BEFF72] text-2xl font-bold tracking-wider'>Category Name</h3>
         </div>
-        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer'>
+        <div className='flex items-center justify-center h-50 w-[28%] bg-[#8B60C3] m-5 rounded-3xl hover:cursor-pointer hover:shadow-[2px_2px_15px] hover:shadow-[#0098B3]'>
             {/* Section Card */}
             <h3 className='text-[#BEFF72] text-2xl font-bold tracking-wider'>Category Name</h3>
         </div>
